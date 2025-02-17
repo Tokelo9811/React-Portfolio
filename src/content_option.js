7d4341af7bbe9c5dd87f6cd599ad7e22c4c4bd27
Mpho Vincent Makgwe <118048918+Mpho-vincent-makgwe@users.noreply.github.com>
2025-02-17 17:03:38 +0200

    Innitiated

---
+++ src/content_option.js
@@ -0,0 +1,112 @@
+const logotext = "Tokelo";
+const meta = {
+    title: "Tokelo Magutle",
+    description: "I’m Tokelo Magutle Software Engineer _ Full stack React Native devloper,currently working in Centurion",
+};
+
+const introdata = {
+    title: "I’m Tokelo Magutle",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "Full-Stack Software Engineer | React Native, React.js, Node.js, Express.js & MongoDB Expert | Cross-Platform App Development",
+    your_img_url: "https://uc24016f115df06ffd12e1b94df9.previews.dropboxusercontent.com/p/thumb/ACh34cXp_pqWnXiugfjSKxU5_JXVwrNs-8bwk1LYNg-V1MM9epJKXaqbCqWwZmcKh_eRsnO7ygzidhWsgeKrXLNWw-9rKJgaN4_GaTA8VuRVa-7TrU9RJCWTS1PRyX4916PL6lGz0rN-X1Eq0h0S1x_aggJjtCLVR7m69K2nyQ0nbagss4Hi8q7M2kS0zTl10V7RcxpICLqIFdmznytS0t4rdcStpwgzWZVHhKW-oDHz-8dVASvtgaz7k706icX7941XJ2aj1LQUnjef3kXKyKRvjp0HhrSYhkONT714ntUwRTNhGMdIN9abXGprIEBP12c9KMIY8m-BBk69KRiLGCiQ2_Fztnidq6j3ML6601lYqYqSdhxTNFQ1k8xQ2CIsm02-zlSgbEGuXbHd7BU9ocjC/p.jpeg?is_prewarmed=true",
+};
+
+const dataabout = {
+    title: "abit about my self",
+    aboutme: "I am a passionate full-stack software developer with over 3 years of hands-on experience specializing in React Native and React.js. My expertise lies in designing and implementing robust, scalable applications, with a proven record of building large-scale platforms, including a high-demand delivery app that prioritizes seamless user experience and efficient performance.",
+};
+const worktimeline = [
+    {
+        jobtitle: "Technical Lead & Full-Stack Developer",
+        where: "Mzxit",
+        date: "2023 - Present",
+    },
+    {
+        jobtitle: "Full-Stack Developer",
+        where: "EatsHub",
+        date: "2019 - 2023",
+    },
+];
+
+
+const skills = [{
+        name: "React Native",
+        value: 90,
+    },
+    {
+        name: "NodeJs",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [
+    {
+        title: "Back-End Developer",
+        description: "Builds and maintains server-side logic, databases, and APIs to ensure seamless application functionality.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Designs and develops mobile applications for Android and iOS platforms, ensuring high performance and user-friendly interfaces.",
+    },
+    {
+        title: "Front-End Developer",
+        description: "Creates responsive and visually appealing user interfaces using modern web technologies like HTML, CSS, and JavaScript.",
+    },
+];
+
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/Tokelo9811/TransApp-Backend",
+    },
+    {
+        img: "https://uca4b2751ec1702ea80c3974a57d.previews.dropboxusercontent.com/p/thumb/AChenGCT9ndwb5Y45sr98Ar-CFoPwAMru3pdstpzv1y2dI72v_SPHBskxLrCVF-OXPbME5j0s4mjIw7fNd-1HLFyWbTq7GT_J-_mT5P6uDHfucD7y8A8bJmsi4Ni9J-Uroh5st4l6rSP4ZRjkWaN_pK5M9o0l5N4D0_Q_v3L5q3XdHiL1HxKepkuLLmgDpsEmPGP_KDRtT5kU0RXBB1reDCsAFL7Az8-frPvxCid8uvUXPEZODZrdXqT8WSblQ5chinMzvorgKeMraSZ1rw9Vg6z5uVThn8Gm8IwSokg7DHJF_crQIIErkKI2lYTp3gz83t4_RC-_4MGwaNrJhFdtpPOK37gYEB7FO9b5PzuygXAP4sVMPG_NAw4cBF7IhlS-cA/p.jpeg?is_prewarmed=true?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/mzxit",
+    },
+    {
+        vid: "https://www.dropbox.com/scl/fi/zx3uind3kwixxf7akjn9d/WhatsApp-Video-2025-02-09-at-14.33.11_f20f4254.mp4?rlkey=p1r5rc9hfsnqv8m68gssjzmlo&st=0ng9ehsr&dl=0",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "https://github.com/mzxit",
+        
+    },
+
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "tokelomagutles3@gmail.com",
+    YOUR_FONE: "(+27)66 377 8921",
+    description: "Get in touch with Tokelo for professional development services. Whether you need backend solutions, mobile app development, or a sleek front-end design, feel free to reach out!",
+    // Create an emailjs.com account 
+    // Check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+
+const socialprofils = {
+    github: "https://github.com/Tokelo9811",
+    linkedin: "https://www.linkedin.com/in/tokelomagutle/",
+};
+export {
+    contactConfig, dataabout,
+    dataportfolio, introdata, logotext, meta, services, skills, socialprofils, worktimeline
+};
